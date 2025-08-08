0bc3f9f84229e170afe3bcee0f336627cd60fdd1
Default <joseencodedeveloper@gmail.com>
2025-08-08 18:58:16 -0500

    feat: add header component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,29 +10,10 @@ const routes: Routes = [
     path: '',
     component: AdminComponent,
     children: [
-      {
-        path: '',
-        redirectTo: '/dashboard/products/list',
-        pathMatch: 'full'
-      },
-      {
-        path: 'dashboard/products/create',
-        loadComponent: () =>
-          import('./presentation/pages/dashboard/product-form/product-form.component')
-            .then((c) => c.ProductFormComponent)
-      },
-      {
-        path: 'dashboard/products/update/:id',
-        loadComponent: () =>
-          import('./presentation/pages/dashboard/product-form/product-form.component')
-            .then((c) => c.ProductFormComponent)
-      },
-      {
-        path: 'dashboard/products/list',
-        loadComponent: () =>
-          import('./presentation/pages/dashboard/product-list/product-list.component')
-            .then((c) => c.ProductListComponent)
-      }
+    { path: 'dashboard/products/list', loadComponent: () => import('./presentation/pages/dashboard/product-list/product-list.component').then(m => m.ProductListComponent) },
+    { path: 'dashboard/products/create', loadComponent: () => import('./presentation/pages/dashboard/product-create/product-create.component').then(m => m.ProductCreateComponent) },
+    { path: 'dashboard/products/edit/:id', loadComponent: () => import('./presentation/pages/dashboard/product-edit/product-edit.component').then(m => m.ProductEditComponent) },
+    { path: '', pathMatch: 'full', redirectTo: '/dashboard/products/list' }
     ]
   }
 ];
